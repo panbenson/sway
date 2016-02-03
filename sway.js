f40b6228d3547dc11c2369221628f4b8345e67fc
Benson Pan <benson.pan13@gmail.com>
2016-02-03 16:34:45 -0500

    Update sway.js
    - Tested all velocity animations; height and width behave unexpectedly
    - Fixed animation 'stacking'

--- sway.js
+++ sway.js
@@ -1,6 +1,6 @@
 /* Sway (c) 2016 Benson Pan. MIT @license: en.wikipedia.org/wiki/MIT_License */
 
-/* Note: Sway relies on Velocity.js, a copy can be found on VelocityJS.org */
+/* Note: Sway relies on JQuery and Velocity.js; a copy can be found on VelocityJS.org */
 var callCount = 0;
 
 var sway = function(animation)
@@ -16,7 +16,7 @@ var sway = function(animation)
 	
 	$('.sway').each(function(i, obj)
 	{
-		if (callCount <= i)
+		if (callCount == i)
 		{
 			var message = $(this).text();
 			notCalled = 0;
@@ -24,7 +24,6 @@ var sway = function(animation)
 			
 			while (message != '')
 			{
-				// retrieve content using .text() instead, in case no js support
 				var indexSpace1 = message.indexOf(' ');
 				if (indexSpace1 != -1)
 				{
@@ -52,28 +51,42 @@ var sway = function(animation)
 				var temp = {};
 				var ending = {};
 				temp[velocity_param] = parseInt(animation[velocity_param]) * -1;
-				ending[velocity_param] = $(this).css(velocity_param);
 				
-				if (velocity_param.indexOf("scale") != -1)
+				
+				if ("scale".indexOf(velocity_param) != -1)
 				{
 					ending[velocity_param] = 1;
 				}
-				if (velocity_param.indexOf("fontSize") != -1)
+				else if ("fontSizeopacity".indexOf(velocity_param) != -1)
 				{
-					console.log($(this).css("font-size"));
-					temp[velocity_param] = 10;
+					ending[velocity_param] = $(this).css(velocity_param);
+					temp[velocity_param] = $(this).css(velocity_param) / 2;
 				}
+				else if ("color".indexOf(velocity_param) != -1)
+				{
+					temp = animation;
+					ending[velocity_param] = rgb2hex($(this).css(velocity_param));
+				}
+				else
+				{
+					ending[velocity_param] = $(this).css(velocity_param);
+				}
+				// FOLLOWING ARE NOT SUPPORTED:
+				if ("heightwidth".indexOf(velocity_param) == -1)
+				{
+					$(this).hover(function()
+					{
+						if ($('.velocity-animating'))
+							$(this).stop();
+						$(this).velocity(animation, {duration:100, easing: 'easeOutBounce'});
+						$(this).velocity(temp, {duration:200, easing: 'easeOutBounce'});
+						$(this).velocity(ending, {duration:250, easing: 'easeOutBounce'});
+					}, 
+					function()
+					{
+					});
+				}	
 				
-					
-				$(this).hover(function()
-				{
-					$(this).velocity(animation, {duration:100, easing: 'easeOutBounce'});
-					$(this).velocity(temp, {duration:200, easing: 'easeOutBounce'});
-					$(this).velocity(ending, {duration:250, easing: 'easeOutBounce'});
-				}, 
-				function()
-				{
-				});
 			});
 			$('.inner-sway' + callCount).css({'display':'inline-block'});
 		}
@@ -82,4 +95,14 @@ var sway = function(animation)
 			console.log("Sway: Function was called more than number of available .sway divs")
 	});
 	callCount++;
+}
+
+function rgb2hex(rgb) {
+    if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
+
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
